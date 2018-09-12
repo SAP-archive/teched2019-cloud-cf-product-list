70c125cda494c76226a485012f55bcdebdd69c3f
Marc Rahn <marc.rahn@sap.com>
2018-09-12 11:15:12 +0200

    Add logging

--- java/src/main/java/com/sap/cp/cf/demoapps/Controller.java
+++ java/src/main/java/com/sap/cp/cf/demoapps/Controller.java
@@ -11,26 +11,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.HttpConstraint;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 
 @WebServlet({ "/products/*", "/productsByParam" })
 // configure servlet to check against scope "$XSAPPNAME.read"
- @ServletSecurity(@HttpConstraint(rolesAllowed = { "read" }))
+@ServletSecurity(@HttpConstraint(rolesAllowed = { "read" }))
 public class Controller extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private Gson gson = new Gson();
 	private ProductService productService = new ProductService();
-	private static final Logger logger = Logger.getLogger(Controller.class);
+	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String productsJson;
 		if (request.getParameter("name") != null) {
-			productsJson = this.gson.toJson(productService.getProductByName(request.getParameter("name")));
-		
+			String name = request.getParameter("name");
+			logger.info("***First - Retrieving details for '{}'.", name);
+			logger.info("***Second - Retrieving details for '{}'.", name);
+			productsJson = this.gson.toJson(productService.getProductByName(name));
+
 		} else {
 			productsJson = this.gson.toJson(productService.getProducts());
 		}
