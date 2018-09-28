70a2439fadda9fc35b6af6b7182e12e8c52b12ac
Marc Rahn <marc.rahn@sap.com>
2018-09-28 09:44:21 +0200

    Remove logging

--- java/src/main/java/com/sap/cp/cf/demoapps/Controller.java
+++ java/src/main/java/com/sap/cp/cf/demoapps/Controller.java
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.HttpConstraint;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.gson.Gson;
 
 @WebServlet({ "/products/*", "/productsByParam" })
@@ -21,28 +18,26 @@ import com.google.gson.Gson;
 @ServletSecurity(@HttpConstraint(rolesAllowed = { "read" }))
 public class Controller extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
-	private Gson gson = new Gson();
-	private ProductService productService = new ProductService();
-	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+    private static final long serialVersionUID = 1L;
+    private Gson gson = new Gson();
+    private ProductService productService = new ProductService();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		String productsJson;
-		if (request.getParameter("name") != null) {
-			String name = request.getParameter("name");
-			logger.info("***First - Retrieving details for '{}'.", name);
-			logger.info("***Second - Retrieving details for '{}'.", name);
-			productsJson = this.gson.toJson(productService.getProductByName(name));
+        String productsJson;
+        if (request.getParameter("name") != null) {
+            String name = request.getParameter("name");
+            productsJson = this.gson.toJson(productService.getProductByName(name));
 
-		} else {
-			productsJson = this.gson.toJson(productService.getProducts());
-		}
+        } else {
+            productsJson = this.gson.toJson(productService.getProducts());
+        }
 
-		response.setContentType("application/json");
-		PrintWriter out = response.getWriter();
-		out.print(productsJson);
-		out.flush();
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print(productsJson);
+        out.flush();
 
-	}
+    }
 }
